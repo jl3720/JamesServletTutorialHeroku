102564ad7729d55c703f61745862768d6b754c70
James <jameszcliu@gmail.com>
2022-11-29 12:43:37 +0000

    Attempted to add html to servlet

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -14,7 +15,12 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write(req.getServletPath());
+//        resp.getWriter().write();
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<html>");
+        printWriter.print("<head>");
+        printWriter.print("<title> This is a title </title>");
+        printWriter.print("</head>");
 
     }
 
